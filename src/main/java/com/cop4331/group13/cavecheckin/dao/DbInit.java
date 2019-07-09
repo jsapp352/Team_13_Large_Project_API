833f99950ccb48bea7c420adca72defdcca8cda2
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-07-09 17:42:49 -0400

    Added admin user creation DbInit

--- src/main/java/com/cop4331/group13/cavecheckin/dao/DbInit.java
+++ src/main/java/com/cop4331/group13/cavecheckin/dao/DbInit.java
@@ -17,12 +17,17 @@ public class DbInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword(passwordEncoder.encode("admin123"));
-        admin.setRole("ADMIN");
-        admin.setActive(true);
+        // If an "admin" user does not exist, create it.
+        if (dao.findByUsername("admin") == null)
+        {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRole("ADMIN");
+            admin.setActive(true);
 
-        // dao.save(admin);
+            dao.save(admin);
+        }
+
     }
 }
