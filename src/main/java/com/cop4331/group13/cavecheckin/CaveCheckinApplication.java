2e33b923317191aaa018a2daab2c99b153bba107
Andy Tschida <andyltschida@gmail.com>
2019-07-17 23:52:23 -0400

    Added updated bean

--- src/main/java/com/cop4331/group13/cavecheckin/CaveCheckinApplication.java
+++ src/main/java/com/cop4331/group13/cavecheckin/CaveCheckinApplication.java
@@ -1,6 +1,12 @@
 package com.cop4331.group13.cavecheckin;
 
+import com.cop4331.group13.cavecheckin.api.dto.user.UserResponseDto;
+import com.cop4331.group13.cavecheckin.config.EncryptionUtil;
+import com.cop4331.group13.cavecheckin.domain.User;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.TypeMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +20,16 @@ public class CaveCheckinApplication {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper mapper =  new ModelMapper();
+		EncryptionUtil util = new EncryptionUtil();
+		mapper.addMappings(new PropertyMap<User, UserResponseDto>() {
+			@Override
+			protected void configure() {
+				map().setKioskPin(Long.parseLong(util.Encrypt(source.getKioskPin())));
+			}
+		});
+
+		return mapper;
 	}
 
 }
