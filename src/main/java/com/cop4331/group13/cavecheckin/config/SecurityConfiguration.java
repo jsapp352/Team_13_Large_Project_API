974157ce5ac99c4bdb309fedc0fee851684ec84f
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-07-19 20:52:51 -0400

    Allow all headers

--- src/main/java/com/cop4331/group13/cavecheckin/config/SecurityConfiguration.java
+++ src/main/java/com/cop4331/group13/cavecheckin/config/SecurityConfiguration.java
@@ -83,7 +83,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedHeaders(ImmutableList.of("*"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
